2ee30b87f480cde38ff98e10d1ab25ecfdc18479
Diego Paixão <34756441+dev-paixao@users.noreply.github.com>
2023-06-17 21:55:15 -0300

    Melhorias no JS e no HTML do encurtador de URL
    Atualização do HTML para melhorar a semântica, renomeando classes para tornar o código mais legível e compreensível.
    Adição do campo "URL encurtada" no HTML para exibir a URL encurtada gerada.
    Ajuste no código JavaScript para adicionar automaticamente "https://" caso não seja fornecido pelo usuário.
    Correção de erros no código JavaScript, incluindo tratamento de erros na requisição para encurtar a URL.

--- js/script.js
+++ js/script.js
@@ -1,43 +1,62 @@
 function encurtarUrl() {
-    //validar se o link existe
-    let url = document.getElementById("input-url").value;
-    if(!url) {
-        alert("É preciso inserir uma URL para encurtar");
-        return;
-    }
-
-    //encurtar o link
-
-    //headers
+    // Headers
     let headers = {
         "Content-Type": "application/json",
         "apiKey": "d7e0ae4f5d834b2b90080076c9712c90"
+    };
+
+    // Dados
+    let inputUrl = document.getElementById("input-url");
+    let url = inputUrl.value;
+
+    // Verificar se o link existe
+    if (!url) {
+        mostrarUrlEncurtada("");
+        return;
     }
 
-    //dados
+    // Adicionar "https://" automaticamente se não estiver presente
+    if (!url.startsWith("https://") && !url.startsWith("http://")) {
+        url = "https://" + url;
+    }
+
     let linkRequest = {
         destination: url,
         domain: { fullName: "rebrand.ly" }
-    }
+    };
 
     fetch("https://api.rebrandly.com/v1/links", {
         method: "POST",
         headers: headers,
         body: JSON.stringify(linkRequest)
     })
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Erro ao encurtar a URL");
+            }
+            return response.json();
+        })
         .then(json => {
-            console.log(json);
-            let inputUrl = document.getElementById("input-url");
-            inputUrl.value = json.shortUrl;
+            let shortUrl = json.shortUrl;
+            mostrarUrlEncurtada(shortUrl);
+        })
+        .catch(error => {
+            console.error(error);
+            mostrarUrlEncurtada("");
         });
 }
 
-function copiar() {
-    let inputUrl = document.getElementById("input-url");
-
-    inputUrl.select();
-    inputUrl.setSelectionRange(0, 99999);
-
-    navigator.clipboard.writeText(inputUrl.value);
+function mostrarUrlEncurtada(urlEncurtada) {
+    let inputUrlEncurtada = document.getElementById("input-url-encurtada");
+    inputUrlEncurtada.value = urlEncurtada;
 }
+
+function copiar() {
+    let inputUrlEncurtada = document.getElementById("input-url-encurtada");
+
+    inputUrlEncurtada.select();
+    inputUrlEncurtada.setSelectionRange(0, 99999);
+
+    navigator.clipboard.writeText(inputUrlEncurtada.value);
+}
+
